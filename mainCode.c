61462f3be72533d3468410e8a8b06e160bbda52c
mmilan331 <mamilan331@gmail.com>
2023-10-10 13:17:53 -0500

    Updated mainCode.c

--- mainCode.c
+++ mainCode.c
@@ -1,30 +1,37 @@
-//Program designed to do [SOMETHING] with file input
-//      and file output.
-
+/* Program designed to do [SOMETHING] with file input
+      and file output.
+*/
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(int argc, char *argv[]){
-    //Check for correct number of arguments
+    /*
+    Check for correct number of arguments
+    */
     if (argc != 3){
         printf("\nUSAGE: --- ./a.out <file input> <file output> ---\n\n");
         return -1;
     }
 
+    /*
+    Open files and check for valid pointer
+    */
     FILE *fin = fopen(argv[1], "r"); //Read file, file must exist
-    FILE *fout = fopen(argv[2], "a"); //Append creates file if it doesn't exist
-
-    //Check if files have opened correctly
     if (fin == NULL){
         fprintf(stderr, "--- Error opening file %s ---\n", argv[1]);
         return -1;
     }
+    FILE *fout = fopen(argv[2], "a"); //Append creates file if it doesn't exist
     if (fout == NULL){
         fprintf(stderr, "--- Error opening file %s ---\n", argv[2]);
     }
     
+    //Do Stuff
 
 
+    //close file descriptors
+    fclose(fin);
+    fclose(fout);
 
     return 0;
 }
