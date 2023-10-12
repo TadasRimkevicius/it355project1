8133985dbb9be278435e8c1a325aa23632d83542
mmilan331 <mamilan331@gmail.com>
2023-10-12 00:52:05 -0500

    Updated mainCode.c, leaning towards wordle game

--- mainCode.c
+++ mainCode.c
@@ -1,10 +1,16 @@
-/* Program designed to do [SOMETHING] with file input
+/* Program designed to do ... wordle...? with file input
       and file output.
 */
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[]){
+ 
+   char* buffer[1024];
+   char words[100];
+   int wordCount = 0;
+
     /*
     Check for correct number of arguments
     */
@@ -18,16 +24,26 @@ int main(int argc, char *argv[]){
     */
     FILE *fin = fopen(argv[1], "r"); //Read file, file must exist
     if (fin == NULL){
-        fprintf(stderr, "--- Error opening file %s ---\n", argv[1]);
+        fprintf(stderr, "\n--- Error opening file %s ---\n\n", argv[1]);
         return -1;
     }
     FILE *fout = fopen(argv[2], "a"); //Append creates file if it doesn't exist
     if (fout == NULL){
-        fprintf(stderr, "--- Error opening file %s ---\n", argv[2]);
+        fprintf(stderr, "\n--- Error opening file %s ---\n\n", argv[2]);
+        return -1;
     }
     
-    //Do Stuff
-
+    /*
+    Read in words from file and store them in words[] array
+    */
+    while (fgets(buffer, sizeof(buffer), fin) != NULL){
+        words[wordCount] = strdup(buffer); //Put word into word array
+        if (words[wordCount] == NULL){
+            fprintf(stderr, "\n--- Error reading line ---\n\n");
+            return -1;
+        }
+        wordCount++;
+    }
 
     //close file descriptors
     fclose(fin);
