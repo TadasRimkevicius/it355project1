673727510c44e03c0428e6ca2296038a8b124ee8
mmilan331 <mamilan331@gmail.com>
2023-10-12 13:42:57 -0500

    Updated mainCode.c

--- mainCode.c
+++ mainCode.c
@@ -7,23 +7,20 @@
 #include <time.h>
 
 int main(int argc, char *argv[]){
-    char* buffer[1024];
-    char words[100];
+    char buffer[1024];
+    char *words[100];
     int wordCount = 0;
     srand(time(NULL)); //Initialize random number generator
     int random;
 
-/*
-Check for correct number of arguments
-*/
+    // Check for correct number of arguments
     if (argc != 3){
         printf("\nUSAGE: --- ./a.out <file input> <file output> ---\n\n");
         return -1;
     }
 
-/*
-Open files and check for valid pointer
-*/
+
+    // Open files and check for valid pointer
     FILE *fin = fopen(argv[1], "r"); //Read file, file must exist
     if (fin == NULL){
         fprintf(stderr, "\n--- Error opening file %s ---\n\n", argv[1]);
@@ -35,11 +32,10 @@ Open files and check for valid pointer
         return -1;
     }
     
-/*
-Read in words from file and store them in words[] array
-*/
+
+    // Read in words from file and store them in words[] array
     while (fgets(buffer, sizeof(buffer), fin) != NULL){
-        words[wordCount] = strdup(buffer); //Put word into word array
+        words[wordCount] = buffer; //Put word into word array
         if (words[wordCount] == NULL){
             fprintf(stderr, "\n--- Error reading line ---\n\n");
             return -1;
@@ -47,15 +43,31 @@ Read in words from file and store them in words[] array
         wordCount++;
     }
 
-/*
-Choose a random word from the list as the wordle word
-*/
+
+    // Choose a random word from the list as the wordle word
     random = rand() % wordCount + 1;
-    char* word[] = words[random];
+    char* word = words[random];
+    int wordLength = strlen(word);
+    int guesses = 5;
 
 /*
-close file descriptors and clean up memory
+Start
 */
+
+    printf("\t\t-=- W O R D L E -=-\n");
+    printf("\t =O=------------=O=------------=O=\n\n");
+    printf("The word has %d letters. You have 5 guesses. Good luck!\n\n", wordLength);
+
+
+    //while (guesses != 0){
+
+    //    guesses--;
+    //}
+
+/*
+End
+*/
+    // Close file descriptors and clean up memory
     fclose(fin);
     fclose(fout);
 
