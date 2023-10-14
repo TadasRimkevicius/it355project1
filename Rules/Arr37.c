c6634b500f7073378f43e1932eff8f1c7de1aa6d
Alek Lossev <70302587+Truffl3z@users.noreply.github.com>
2023-10-14 15:44:56 -0500

    Update Arr37.c

--- Rules/Arr37.c
+++ Rules/Arr37.c
@@ -1,27 +1,12 @@
 #include <stdio.h>
-#include <string.h>
 
 int main() {
-    FILE *file = fopen("data.txt", "wb"); // Open a binary file for writing
+    int value = 42;
+    int *ptr = &value;
     
-    if (file == NULL) {
-        perror("fopen");
-        return 1;
-    }
-
-    char data[] = "This is a data with a null character \0 in the middle.";
-    size_t data_len = sizeof(data) - 1; // Exclude the null character
-
-    size_t bytes_written = fwrite(data, 1, data_len, file);
+    // Adding an integer to a pointer to a non-array object
+    int *newPtr = ptr + 1;
     
-    if (bytes_written != data_len) {
-        perror("fwrite");
-        fclose(file);
-        return 1;
-    }
-
-    fclose(file);
-    printf("Data has been written successfully.\n");
-    
+    printf("Value: %d\n", *newPtr);  // Undefined behavior
     return 0;
 }
